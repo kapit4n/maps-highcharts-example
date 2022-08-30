2fa826bfdadefd44546089bd3717e835b28dfeb3
Luis Arce <luis.arce22@gmail.com>
2022-08-30 18:01:36 -0400

    Example map implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import MapExample from './MapExample';
 
 function App() {
+
+  const [topology, setTopology] = useState(null)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    async function example() {
+      const t = await fetch(
+        'https://code.highcharts.com/mapdata/custom/world.topo.json'
+      ).then(response => response.json());
+  
+      setTopology(t)
+      console.log(t)
+  
+      const d = await fetch('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population.json').then(response => response.json());
+      console.log(d)
+      setData(d)
+  
+      setLoading(false)
+    }
+
+    example()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading && (
+        <MapExample
+          topology={topology}
+          data={data}
+        />
+      )}
     </div>
   );
 }
---
+++ src/MapExample.js
@@ -0,0 +1,70 @@
+import { useMemo } from 'react'
+
+import Highcharts from "highcharts";
+import HighchartsMap from "highcharts/modules/map";
+import HighchartsReact from "highcharts-react-official";
+
+HighchartsMap(Highcharts);
+
+function MapExample({ data, topology }) {
+
+  const options = useMemo(() => {
+    return {
+      chart: {
+        borderWidth: 1,
+        map: topology
+      },
+
+      title: {
+        text: 'World population 2013 by country'
+      },
+
+      subtitle: {
+        text: 'Demo of Highcharts map with bubbles'
+      },
+
+      accessibility: {
+        description: 'We see how China and India by far are the countries with the largest population.'
+      },
+
+      legend: {
+        enabled: false
+      },
+
+      mapNavigation: {
+        enabled: true,
+        buttonOptions: {
+          verticalAlign: 'bottom'
+        }
+      },
+
+      series: [{
+        name: 'Countries',
+        color: '#E0E0E0',
+        enableMouseTracking: false
+      }, {
+        type: 'mapbubble',
+        name: 'Population 2016',
+        joinBy: ['iso-a3', 'code3'],
+        data: data,
+        minSize: 4,
+        maxSize: '12%',
+        tooltip: {
+          pointFormat: '{point.properties.hc-a2}: {point.z} thousands'
+        }
+      }]
+    }
+  }, [topology, data])
+
+  return (
+    <div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        constructorType={"mapChart"}
+      />
+    </div>
+  );
+}
+
+export default MapExample;
