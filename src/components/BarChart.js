dbfbcc6c6740bfa9b5ac26b375c739d86a5777d8
Luis Arce <luis.arce22@gmail.com>
2022-09-08 19:17:04 -0400

    charts: barchart adding labels

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -6,6 +6,7 @@ class BarChart extends React.PureComponent {
     constructor(props) {
         super(props)
         this.chartRef = React.createRef()
+        this.labelsRef = React.createRef()
         this.dataset = [100, 200, 300, 400, 500]
     }
 
@@ -14,27 +15,41 @@ class BarChart extends React.PureComponent {
 
         let size = 500
 
-        let svg = d3.select(this.chartRef.current)
+        const container = d3.select(this.chartRef.current)
+
+        let svg = container
             .append('svg')
             .attr('width', size)
             .attr('height', size)
 
+        let labels = container
+            .append('div')
+            .attr('width', size)
+            .attr('height', 20)
+            .style("display", 'flex')
+
         let rect_width = 95
 
-        svg.selectAll('rect')
-            .data(this.dataset)
-            .enter()
-            .append('rect')
-            .attr('x', (d, i) => 5 + i*(rect_width + 5))
-            .attr('y', d => size - d)
-            .attr('width', rect_width)
+         svg.selectAll('rect')
+             .data(this.dataset)
+             .enter()
+             .append('rect')
+             .attr('x', (d, i) => 5 + i * (rect_width + 5))
+             .attr('y', d => size - d)
+             .attr('width', rect_width)
+             .attr('height', d => d)
+             .attr('fill', 'lightblue')
+
+        labels.selectAll('p').data(this.dataset).enter().append("p").text(() => "Text")
+            .style('width', `${rect_width + 5}px`)
             .attr('height', d => d)
-            .attr('fill', 'teal')
+            .attr('y', d => size - d)
+
     }
 
     render() {
         return (
-            <div ref={this.chartRef}>
+            <div ref={this.chartRef} style={{display: "block"}}>
             </div>
         )
     }
