84ef6f970dc240d6c5985167aadc4d31b12e2e83
Luis Arce <luis.arce22@gmail.com>
2022-09-08 20:28:01 -0400

    charts: another way to append texts

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -6,15 +6,13 @@ class BarChart extends React.PureComponent {
     constructor(props) {
         super(props)
         this.chartRef = React.createRef()
+        this.secondChartRef = React.createRef()
         this.labelsRef = React.createRef()
         this.dataset = [{ value: 500, label: 'RED', color: 'red' }, { value: 300, label: 'YELLOW', color: 'yellow' }, { value: 100, label: 'GREEN', color: 'green' }]
     }
 
-    componentDidMount() {
-        console.log(this.chartRef)
-
+    buildFirstChart() {
         let size = 500
-
         const container = d3.select(this.chartRef.current)
 
         let svg = container
@@ -71,13 +69,65 @@ class BarChart extends React.PureComponent {
                     .style("opacity", 1);
 
             })
+    }
 
+    buildSecondChart() {
+        let size = 500
+        const container = d3.select(this.chartRef.current)
+        let rect_width = 100
+
+        let svg = container
+            .append('svg')
+            .attr('width', size)
+            .attr('height', size)
+        svg.selectAll('rect')
+            .data(this.dataset)
+            .enter()
+            .append('rect')
+            .attr('x', (d, i) => 5 + i * (rect_width + 5))
+            .attr('y', d => size - d.value)
+            .attr('width', rect_width)
+            .attr('height', d => d.value - 50)
+            .attr('fill', d => d.color)
+            .on("mouseover", function (d) {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style("opacity", 0.5);
+            })
+            .on("mouseout", function (d) {
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style("opacity", 1);
+            });
+
+        var texts = svg.selectAll(".myTexts")
+            .data(this.dataset)
+            .enter()
+            .append("text");
+
+        texts.attr('x', (d, i) => 5 + i * (rect_width + 5))
+            .attr('y', d => size)
+            .text(function (d) { return d.label });
+
+    }
+
+    componentDidMount() {
+       // this.buildFirstChart()
+        this.buildSecondChart()
     }
 
     render() {
         return (
-            <div ref={this.chartRef} style={{ display: "block" }}>
-            </div>
+            <>
+                <div ref={this.chartRef}>
+                </div>
+
+                <div ref={this.secondChartRef}>
+                </div>
+
+            </>
         )
     }
 }
