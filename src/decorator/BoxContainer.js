3a2417ca9bf5ec9b1d051a5ab890d991b9d32f01
Luis Arce <luis.arce22@gmail.com>
2022-09-29 22:08:34 -0400

    Print 10 items

--- src/decorator/BoxContainer.js
+++ src/decorator/BoxContainer.js
@@ -4,12 +4,28 @@ const Box = ({ component }) => {
 
 
   const [check, setCheck] = React.useState(true)
+  const [print10, setPrint10] = React.useState(false)
+  const fileItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   return (
     <div>
-      <input value={check} defaultChecked={check} onChange={() => setCheck(!check)} type="checkbox" />
+      Display: <input value={check} defaultChecked={check} onChange={() => setCheck(!check)} type="checkbox" />
+      Print 10: <input value={print10} defaultChecked={print10} onChange={() => setPrint10(!print10)} type="checkbox" />
       {check && (
-        <>{component}</>
+        <>
+          {print10 ? (
+            <div>
+              {
+                fileItems.map(n => (
+                  <div key={n}>
+                    {n}: {component}
+                  </div>
+                ))
+              }
+            </div>
+          ) : (<>{component}</>)}
+          {component}
+        </>
       )}
     </div>
   )
